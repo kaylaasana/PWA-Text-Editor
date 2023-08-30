e6592698cd1fc07f0b687bfba911eed7aef92579
kayla <kayladatte@gmail.com>
2023-08-29 19:02:06 -0700

    fixed bug with saving text

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -26,7 +26,7 @@ export const putDb = async (content) => {
   const store = tx.objectStore('jate');
 
   // Use the .add() method on the store and pass in the content.
-  const request = store.add({ id: 1, content: content });
+  const request = store.put({ id: 1, value: content });
 
   // Get confirmation of the request.
   const result = await request;
@@ -52,7 +52,7 @@ export const getDb = async () => {
   // Get confirmation of the request.
   const result = await request;
   console.log('result.value', result);
-  return result.content;
+  return result?.value;
 };
 
 initdb();
